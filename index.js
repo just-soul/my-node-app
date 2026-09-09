4e3e46c2a328d293745b59eda7382ed3e8f17efb
misogyny <24winds74@gmail.com>
2026-09-09 17:59:42 +0300

    feat: Add event-driven server, logger

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 const http = require('http');
 
-const fio = "Дема Илья Максимович";
+const fio = "";
 const group = 478;
-const journal_number = 6;  
+const journal_number = 6; 
 
+const EventEmitter = require('events');
+const fs = require('fs');
+const {setupLogger} = require('./logger')
+class AppServer extends EventEmitter{
+	start(port){
+		this.server = http.createServer((req, res)=>{
+			setTimeout(()=>{
+				this.emit('request:received',{url: req.url, method: req.method});},2000);
+				res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+			 	res.end(`
+    				<p>ФИО: ${fio}</p>
+					<p>ГРУППА: ${group}</p>
+					<p>ЧИСЛО ПИ:  ${journal_number} ${piValue}</p>
+    				`);
+    				});
+
+		this.server.listen(port, ()=>{
+			this.emit('server:started', port);});
+		}
+		stop(){
+			setTimeout(()=>{
+				this.server.close(()=>{
+					this.emit('server:stopped');
+					});
+
+				},10000);
+			}	
+		}	
+  
 function PI(precision) {
 	const pi = 355/113
 	return pi.toFixed(precision);
 }
 const piValue = PI(journal_number);
 
-
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end(`
-    	<p>ФИО: ${fio}</p>
-		<p>ГРУППА: ${group}</p>
-		<p>ЧИСЛО ПИ: ${journal_number} ${piValue}</p>
-    	`);
+const server = new AppServer();
+setupLogger(server);
+server.on('server:started',(port)=>{
+	console.log(`Сервер запущен на порту: ${port}`);
+}); 
+server.on('request:received',(req) =>{
+	console.log(`Получен запрос: ${req.url} ${req.method}`);
 });
-
-server.listen(3000, () => {
-    console.log('Сервер запущен на http://localhost:3000');
+server.on('server:stopped',()=>{
+	console.log(`Сервер остановлен`);
 });
+server.start(8080);
+server.stop();
+    //   http://localhost:8080
+
